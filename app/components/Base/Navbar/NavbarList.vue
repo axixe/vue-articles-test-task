9bd09b5b97b56cc5550864259282e442c37775f3
axixe <s1ngleez@yandex.ru>
2025-08-07 21:57:12 +0300

    feat(NavbarList): create component

---
+++ app/components/Base/Navbar/NavbarList.vue
@@ -0,0 +1,29 @@
+<template>
+  <nav class="navbar">
+    <ul class="navbar__list">
+      <li v-for="item in list" class="navbar__item">
+        <NavbarListItem :title="item.title" />
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<script setup lang="ts">
+import NavbarListItem from "~/components/Base/Navbar/NavbarListItem.vue";
+import type { NavbarItem } from "~/global/types/Navbar/NavbarItem";
+
+interface Props {
+  list: NavbarItem[]
+}
+
+defineProps<Props>()
+</script>
+
+<style scoped lang="scss">
+.navbar {
+  &__list {
+    display: flex;
+    gap: 58px;
+  }
+}
+</style>
