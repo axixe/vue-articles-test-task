84802483ef5e3ed9ccca894d090455bdb1f9c2cd
axixe <s1ngleez@yandex.ru>
2025-08-07 21:57:23 +0300

    feat(BaseButton): create component

---
+++ app/components/Base/Button/BaseButton.vue
@@ -0,0 +1,16 @@
+<template>
+  <button type="button" class="button caption-p">
+    <slot />
+  </button>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped lang="scss">
+.button {
+  padding: $indent-l $indent-2xl;
+  border-radius: 12px;
+  background: $color-base;
+  color: $color-white;
+}
+</style>
