d1a4e49f1dbbd9e7193fbc911a642af48841ea34
axixe <s1ngleez@yandex.ru>
2025-08-08 11:50:35 +0300

    feat(BaseLayout): implement footer to layout

--- app/components/Base/Layout/BaseLayout.vue
+++ app/components/Base/Layout/BaseLayout.vue
@@ -5,12 +5,15 @@
     <main class="inner">
       <slot />
     </main>
+
+    <TheFooter />
   </div>
 </template>
 
 <script setup lang="ts">
-
 import TheHeader from "~/components/Header/TheHeader.vue";
+import TheFooter from "~/components/Footer/TheFooter.vue";
+
 </script>
 
 <style scoped lang="scss">
@@ -20,6 +23,7 @@ import TheHeader from "~/components/Header/TheHeader.vue";
   min-height: 100vh;
 
   .inner {
+    flex: 1 0 auto;
     padding: 0 112px;
     background: #FFFFFF;
   }
