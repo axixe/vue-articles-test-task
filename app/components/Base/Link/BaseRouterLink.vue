ad5643341393337a10ab8af26ca4ed9279c30285
axixe <s1ngleez@yandex.ru>
2025-08-09 21:48:57 +0300

    feat(BaseRouterLink): create component

---
+++ app/components/Base/Link/BaseRouterLink.vue
@@ -0,0 +1,19 @@
+<template>
+  <NuxtLink :to="link" >
+    <slot />
+  </NuxtLink>
+</template>
+
+<script setup lang="ts">
+import type { RouteLocationRaw } from "#vue-router";
+
+interface Props {
+  link: RouteLocationRaw;
+}
+
+defineProps<Props>()
+</script>
+
+<style scoped lang="scss">
+
+</style>
