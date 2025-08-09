a8c43b343d29e9f20165b73bca519b63c75a8cf5
axixe <s1ngleez@yandex.ru>
2025-08-09 21:53:21 +0300

    feat(BaseButton): add new button styles

--- app/components/Base/Button/BaseButton.vue
+++ app/components/Base/Button/BaseButton.vue
@@ -1,16 +1,55 @@
 <template>
-  <button type="button" class="button caption-p">
+  <button
+    type="button"
+    class="button caption-p"
+    :class="{
+      [`button--${color}`]: color,
+    }"
+  >
     <slot />
   </button>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ColorType } from "~/global/enums/ColorType";
+
+interface Props {
+  color?: ColorType
+}
+
+withDefaults(defineProps<Props>(), {
+  color: ColorType.Base
+})
+</script>
 
 <style scoped lang="scss">
 .button {
   padding: $indent-l $indent-2xl;
   border-radius: 12px;
-  background: $color-base;
-  color: $color-white;
+  transition:
+    background 0.3s ease,
+    color 0.3s ease;
+
+  &:hover {
+    border-color: transparent;
+    background: $color-grey;
+  }
+
+  &--base,
+  &:active {
+    background: $color-base;
+    color: $color-white;
+  }
+
+  &--grey-light {
+    background: $color-grey-light;
+    color: $color-base;
+  }
+
+  &--white {
+    border: 1px solid $color-grey;
+    background: $color-white;
+    color: $color-base;
+  }
 }
 </style>
