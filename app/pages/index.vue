869f9cf0659d7006a05b6f1820e394432a3ccd68
axixe <s1ngleez@yandex.ru>
2025-08-09 21:55:56 +0300

    feat(index): add help button in index page

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,9 +1,19 @@
 <template>
-  <div>
-
+  <div class="page-home">
+    <BaseRouterLink :link="'/articles-list'">
+      <BaseButton>go to test task</BaseButton>
+    </BaseRouterLink>
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import BaseRouterLink from "~/components/Base/Link/BaseRouterLink.vue";
+</script>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.page-home {
+  display: flex;
+  justify-content: center;
+  padding: 200px 0;
+}
+</style>
