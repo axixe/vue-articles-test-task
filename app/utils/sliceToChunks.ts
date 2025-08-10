dd0406a036ffbc271c424e692e5947ed2d33499b
axixe <s1ngleez@yandex.ru>
2025-08-10 23:29:21 +0300

    feat(sliceToChunks): create util

---
+++ app/utils/sliceToChunks.ts
@@ -0,0 +1,9 @@
+export const sliceToChunks = <T>(array: T[], chunkSize: number): T[][] => {
+    const chunks: T[][] = []
+
+    for (let i = 0; i < array.length; i += chunkSize) {
+        chunks.push(array.slice(i, i + chunkSize))
+    }
+
+    return chunks
+}
